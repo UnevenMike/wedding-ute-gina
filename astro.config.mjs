108b192106c68a99147d68a61b4a90ad17487ca9
Michael Butterfield <michaelbutterfield1996@gmail.com>
2024-01-11 11:26:46 -0600

    Add Tailwind.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import vercelStatic from '@astrojs/vercel/static';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
-    adapter: vercelStatic(),
+  output: 'static',
+  adapter: vercelStatic(),
+  integrations: [tailwind()]
 });
