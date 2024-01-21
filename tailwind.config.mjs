e2aa812583029da132e301a329b354099fcf93b4
michael <michaelbutterfield1996@gmail.com>
2024-01-20 19:17:47 -0600

    Add fonts and save the date.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			'fancy': ['Allenoire'],
+			'plain': ['Novecento'],
+			'normal': ['system-ui', 'sans-serif']
+		},
+		colors: {
+			'wedding-cream': '#f9f5ec',
+			'wedding-purple': '#94838b'
+		},
 		extend: {},
 	},
 	plugins: [],
